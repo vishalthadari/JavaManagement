2c0df7e4cbc55a84797de9af6ed940b1dbca894e
vishal-thadari <vishalthadari@gmail.com>
2021-12-07 00:33:54 -0600

    first commit

---
+++ Bank_Project/src/FirstBank.java
@@ -0,0 +1,10 @@
+
+public class FirstBank {
+	public static void main(String[] args) {
+
+	Account tim = new Account("Tim Statler", "A00001");
+	Account beyonce = new Account("Beyonce", "B00002");
+	beyonce.showMenu();
+	}
+
+}
